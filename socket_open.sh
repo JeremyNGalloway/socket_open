a33a06fc0045547b4f466a6c6ec13e53ee032218
Jeremy Galloway <JeremyNGalloway@gmail.com>
2014-10-10 16:06:25 -0500

    added inet check via mubix

--- socket_open.sh
+++ socket_open.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-#simple shell script to test if the current user can open sockets and bind bash
+#simple shell script to test if the current user can open sockets, bind bash, and egress out
 #xgermx still outchea
 
 SCKT_PT=3306
@@ -15,7 +15,14 @@ timeout 5 bash -c "cat < /dev/null > /dev/tcp/0.0.0.0/${SCKT_PT}"
 SUCCESS=$?
 
 if [ ${SUCCESS} = 0 ]; then
-        echo "Socket successfully opened and closed on port ${SCKT_PT}"
+        echo "Socket successfully opened and closed on localhost via port ${SCKT_PT}"
+        echo "GET /" | nc yo.letmeoutofyour.net ${SCKT_PT}
+        SUCCESS_INET=$?
+                if [ ${SUCCESS_INET} = 0 ]; then
+                        echo "Socket successfully opened and closed on internet host via port ${SCKT_PT}"
+                else
+                        echo "Failed to connect to ${SCKT_PT} on internet host. Server/network may be firewalled."
+                fi
 else
         echo "Failed to open socket on port ${SCKT_PT}"
 fi
